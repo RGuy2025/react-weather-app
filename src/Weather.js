82656c0bc7ac9e73beb25c764f06a5338fb5d785
RGuy2025 <r.s.guyett@gmail.com>
2025-07-03 15:47:13 -0400

    removed unnecessary files, added api for live data

--- src/Weather.js
+++ src/Weather.js
@@ -1,59 +1,80 @@
+import React, { useState } from "react";
 import axios from "axios";
-
 import "./Weather.css";
 
 export default function Weather() {
-  const apiKey = "5912o91beb33d634bfd91ta0a18fa0bd";
-  let apiUrl =
-    `https://api.shecodes.io/weather/v1/current?query={query}&key=${apiKey}&units=metric`;
-  return (
-    <div className="main-body">
-      <header>
-        <div className="row">
-          <form className="search-form">
-            <input
-              type="search"
-              placeholder="Enter a city"
-              required
-              className="search-form-input"
-            />
-            <input
-              type="submit"
-              value="Search"
-              className="search-form-button"
-            />
-          </form>
-        </div>
-      </header>
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      ready: true,
+      city: response.data.city,
+      date: "Thursday July 3rd, 15:30",
+      temperature: Math.round(response.data.temperature.current),
+      description: response.data.condition.description,
+      humidity: response.data.temperature.humidity,
+      feels: Math.round(response.data.temperature.feels_like),
+      iconUrl: `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`,
+    });
+  }
+  if (weatherData.ready) {
+    return (
+      <div className="main-body">
+        <header>
+          <div className="row">
+            <form className="search-form">
+              <input
+                type="search"
+                placeholder="Enter a city"
+                required
+                className="search-form-input"
+              />
+              <input
+                type="submit"
+                value="Search"
+                className="search-form-button"
+              />
+            </form>
+          </div>
+        </header>
 
-      <div className="row weather-app-data">
-        <div className="col-6">
-          <h1 className="weather-app-city">Brantford</h1>
-          <p className="weather-app-details">
-            <span>10:10</span>, <span>sunny</span>
-            <br />
-            Humidity:
-            <span className="humidity-details">5</span>, Wind:{" "}
-            <span className="wind-details">10 </span>
-          </p>
-        </div>
+        <div className="row weather-app-data">
+          <div className="col-6">
+            <h1 className="weather-app-city">{weatherData.city}</h1>
+            <p className="weather-app-details">
+              <span className="date-details">{weatherData.date}</span>,{" "}
+              <span className="description-details">{weatherData.description}</span>
+              <br />
+              Humidity:
+              <span className="humidity-details">{weatherData.humidity}</span>%,
+              Feels Like:
+              <span className="feels-like-details">{weatherData.feels}</span>°
+            </p>
+          </div>
 
-        <div className="col-6">
-          <div className="weather-app-temperature-container">
-            <img
-              src="https://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png"
-              alt="#"
-            />
+          <div className="col-6">
+            <div className="weather-app-temperature-container">
+              <img src={weatherData.iconUrl} alt={weatherData.description} />
 
-            <div className=" weather-app-temperature">18</div>
-            <span className=" weather-app-unit">
-              <a href="#temp">°C </a> | <a href="#temp">°F</a>
-            </span>
+              <div className=" weather-app-temperature">
+                {weatherData.temperature}
+              </div>
+              <span className=" weather-app-unit">
+                <a href="#temp">°C </a> | <a href="#temp">°F</a>
+              </span>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="row weather-forecast"></div>
-    </div>
-  );
+        <div className="row weather-forecast"></div>
+      </div>
+    );
+  } else {
+    const apiKey = "5912o91beb33d634bfd91ta0a18fa0bd";
+    let city = "Brantford";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
+  }
 }
