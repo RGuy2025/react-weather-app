914263abbbee695f630f0d3911e79726745fd446
RGuy2025 <r.s.guyett@gmail.com>
2025-07-15 09:17:36 -0400

    removed a line from a couple of files to fix deployment issues

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -1,4 +1,4 @@
-import React from "react";
+
 import WeatherForecastIcon from "./WeatherForecastIcon";
 
 export default function WeatherForecastDay(props) {
--- src/WeatherForecastIcon.js
+++ src/WeatherForecastIcon.js
@@ -1,4 +1,4 @@
-import React from "react";
+
 
 export default function WeatherForecastIcon(props) {
   let forecastIconUrl = `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${props.condition.icon}.png`;
